830ebf4a974ff6111d07eeae90852886a6a436bd
aimanayesha09 <ayesha.aiman@elev.ga.ntig.se>
2026-04-16 14:20:28 +0200

    Lade till css stilar för websidan och lade även javascript med canvas klocka

--- includes/footer.php
+++ includes/footer.php
@@ -2,10 +2,7 @@
     <footer class="footer">
         <div class="container">
             <p>Skolprojekt 2026</p>
-            <p>
-        <a href="https://github.com/yourusername" target="_blank">GitHub</a> |
-        <a href="mailto:you@example.com">Contact</a>
-    </p>
+        
         </div>
     </footer>
     
